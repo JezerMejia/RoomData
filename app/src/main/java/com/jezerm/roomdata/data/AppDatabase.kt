7626660f01cb8dc70bb52cccf5f50238fe46345b
Jezer Mejía <amyuki4@icloud.com>
2022-11-14 01:32:15 -0600

    Added database

---
+++ app/src/main/java/com/jezerm/roomdata/data/AppDatabase.kt
@@ -0,0 +1,24 @@
+package com.jezerm.roomdata.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.jezerm.roomdata.entities.Student
+import com.jezerm.roomdata.entities.StudentDAO
+
+@Database(entities = [Student::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun studentDao(): StudentDAO
+
+    companion object {
+        private var instance: AppDatabase? = null
+
+        fun getInstance(context: Context) : AppDatabase {
+            if (instance == null) {
+                instance = Room.databaseBuilder(context, AppDatabase::class.java, "college").build()
+            }
+            return instance!!
+        }
+    }
+}
