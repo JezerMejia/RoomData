435bca3520f1a68bdc5f4165ccc942cc809dbf62
Jezer Mejía <amyuki4@icloud.com>
2022-11-15 18:13:14 -0600

    Added a RecyclerView to preview all students

---
+++ app/src/main/java/com/jezerm/roomdata/entities/StudentAdapter.kt
@@ -0,0 +1,33 @@
+package com.jezerm.roomdata.entities
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.jezerm.roomdata.databinding.StudentItemBinding
+
+class StudentAdapter(var list: List<Student>) : RecyclerView.Adapter<StudentAdapter.StudentHolder>() {
+
+    inner class StudentHolder(private val binding: StudentItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun load(student: Student) {
+            with(binding) {
+                tvName.text = student.nombreCompleto
+                tvDetails.text = "${student.id} - ${student.carrera}"
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudentHolder {
+        val binding = StudentItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return StudentHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: StudentHolder, position: Int) {
+        holder.load(list[position])
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+}
--- app/src/main/java/com/jezerm/roomdata/ui/request/RequestDataActivity.kt
+++ app/src/main/java/com/jezerm/roomdata/ui/request/RequestDataActivity.kt
@@ -2,9 +2,12 @@ package com.jezerm.roomdata.ui.request
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.divider.MaterialDividerItemDecoration
 import com.jezerm.roomdata.data.AppDatabase
 import com.jezerm.roomdata.databinding.ActivityRequestDataBinding
 import com.jezerm.roomdata.entities.Student
+import com.jezerm.roomdata.entities.StudentAdapter
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
@@ -24,19 +27,13 @@ class RequestDataActivity : AppCompatActivity() {
             launch {
                 val students = studentDao.getAll()
                 println(students)
-
-//                studentDao.insert(
-//                    Student(
-//                        12,
-//                        "Juan",
-//                        "",
-//                        "Pérez",
-//                        "Rodríguez",
-//                        "ISI",
-//                        20
-//                    )
-//                )
+                initData(students)
             }
         }
+    }
+
+    private fun initData(students: List<Student>) {
+        binding.rcvStudentList.layoutManager = LinearLayoutManager(this)
+        binding.rcvStudentList.adapter = StudentAdapter(students)
     }
 }
